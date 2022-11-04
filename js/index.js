cabd814d344f056b2924830921f734a620a69a90
Andy Schwedler <a.schwedler@me.com>
2022-11-04 14:15:46 +0100

    add function to bookmark

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 console.clear();
 
+// Add bookmark-background
 const bookmark = document.querySelector('[data-js="bookmark-button"]');
 const bookmarkContainer = document.querySelector('[data-js="bookmark-button"]');
 
 bookmarkContainer.addEventListener("click", (event) => {
   bookmark.classList.toggle("question-card--container__bookmark--clicked");
 });
+// Add bookmark-background
