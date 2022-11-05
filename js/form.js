a50840ddabd84f8dd0e966a723126b099eef0422
Andy Schwedler <a.schwedler@me.com>
2022-11-05 22:04:19 +0100

    add code to form page

--- js/form.js
+++ js/form.js
@@ -1,23 +1,55 @@
 const form = document.querySelector('[data-js="form"]');
 const submitButton = document.querySelector('[data-js="submit-button"]');
-const createdCardList = document.querySelector('[data-js="createdCards"]');
 
+//store user entries in a variable
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  const question = form.question.value;
-  const answer = form.answer.value;
 
+  console.log(event);
+  const questionInput = form.question.value;
+  const answerInput = form.answer.value;
+
+  const section = document.createElement("section");
+  form.append(section);
+  section.classList.add("question-card--flex-container");
+
+  const question = document.createElement("p");
+  section.append(question);
+  question.classList.add("question-card--flex-container__question");
+  question.textContent = questionInput;
+
+  const toggleButton = document.createElement("button");
+  question.append(toggleButton);
+  toggleButton.classList.add("question-card--flex-container__button");
+
+  const answer = document.createElement("p");
+  toggleButton.append("answer");
+  answer.classList.add("question-card--flex-container__answer--hidden");
+
+  //reset and focus on next card question
   event.target.elements.question.focus();
   event.target.reset();
 });
 
-function addTask(question, answer) {
-  const newCard = document.createElement("li");
-  createdCardList.append(newCard);
-  const newQuestion = document.createElement("p");
-  newQuestion.textContent = question;
-  const newAnswer = document.createElement("p");
-  newAnswer.textContent = answer;
-  newCard.append(newQuestion);
-  newCard.append(newAnswer);
-}
+// <p class="question-card--flex-container__question">
+//   What is the Undertakers real name?
+// </p>
+// <button
+//   type="button"
+//   class="question-card--flex-container__button"
+//   data-js="answer-button"
+// >
+//   show answer
+// </button>
+// <p
+//   class="question-card--flex-container__answer--hidden"
+//   data-js="answerField"
+// >
+//   Mark W. Calaway
+// </p>
+// <ul class="question-card--flex-container__tags">
+//   <li class="question-card--flex-container__tags__item">#css</li>
+//   <li class="question-card--flex-container__tags__item">#flexbox</li>
+//   <li class="question-card--flex-container__tags__item">#html</li>
+// </ul>
+// </section>
