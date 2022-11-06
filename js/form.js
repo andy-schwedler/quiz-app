dd94de29e98fdbceced5ec60c5f670296c4c2e6e
Andy Schwedler <a.schwedler@me.com>
2022-11-06 22:01:30 +0100

    add tags to created card

--- js/form.js
+++ js/form.js
@@ -8,9 +8,11 @@ form.addEventListener("submit", (event) => {
   console.log(event);
   const questionInput = form.question.value;
   const answerInput = form.answer.value;
+  const tagsInput = form.tags.value;
 
+  // if no Input has been made
   const section = document.createElement("section");
-  form.append(section);
+  document.body.append(section);
   section.classList.add("question-card--flex-container");
 
   const createdQuestion = document.createElement("p");
@@ -19,9 +21,15 @@ form.addEventListener("submit", (event) => {
   createdQuestion.textContent = questionInput;
 
   const createdAnswer = document.createElement("p");
-  p.append(createdAnswer);
+  section.append(createdAnswer);
   createdAnswer.classList.add("show-answer");
   createdAnswer.textContent = answerInput;
+
+  // const createdTagsContainer = document.createElement("ul");
+  // section.append(createdTagsContainer);
+
+  // const createdTagsInput = document.createElement("li");
+  // ul.append(createdTagsInput);
 
   //reset and focus on next card question
   event.target.elements.question.focus();
