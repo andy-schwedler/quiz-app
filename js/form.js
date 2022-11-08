ff4c6c24310b329c4da00284ff81181aed7049f1
Andy Schwedler <a.schwedler@me.com>
2022-11-08 21:51:02 +0100

    add structure to form js

--- js/form.js
+++ js/form.js
@@ -1,38 +1,28 @@
+console.clear();
+
 const form = document.querySelector('[data-js="form"]');
 const submitButton = document.querySelector('[data-js="submit-button"]');
 
-//store user entries in a variable
+function createQuizCard(question, answerInput) {
+  const section = document.createElement("section");
+  section.classList.add("question-card--flex-container");
+  document.body.append(section);
+
+  const createdQuestion = document.createElement("p");
+  createdQuestion.classList.add("question-card--flex-container__question");
+  createdQuestion.textContent = question;
+  section.append(createdQuestion);
+
+  const createdAnswer = document.createElement("p");
+  createdAnswer.classList.add("show-answer");
+  createdAnswer.textContent = answerInput;
+  section.append(createdAnswer);
+}
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  console.log(event);
-  const questionInput = form.question.value;
-  const answerInput = form.answer.value;
-  const tagsInput = form.tags.value;
-
-  // if no Input has been made
-  const section = document.createElement("section");
-  document.body.append(section);
-  section.classList.add("question-card--flex-container");
-
-  const createdQuestion = document.createElement("p");
-  section.append(createdQuestion);
-  createdQuestion.classList.add("question-card--flex-container__question");
-  createdQuestion.textContent = questionInput;
-
-  const createdAnswer = document.createElement("p");
-  section.append(createdAnswer);
-  createdAnswer.classList.add("show-answer");
-  createdAnswer.textContent = answerInput;
-
-  // const createdTagsContainer = document.createElement("ul");
-  // section.append(createdTagsContainer);
-  // createdTagsContainer.classList.add("question-card--flex-container__tags");
-
-  // const createdTagsInput = document.createElement("li");
-  // ul.append(createdTagsInput);
-  // createdTagsInput.classList.add("question-card--flex-container__tags__item");
-  // createdTagsInput.textContent = createdTagsInput;
+  createQuizCard(form.question.value, form.answer.value);
 
   event.target.elements.question.focus();
   event.target.reset();
