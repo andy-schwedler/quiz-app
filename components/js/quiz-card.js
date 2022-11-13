1e5a561948d4313d4756842f85a3f6bef892f8c1
Andy Schwedler <116001730+andy-schwedler@users.noreply.github.com>
2022-11-13 21:31:23 +0100

    add rick and morty

--- components/js/quiz-card.js
+++ components/js/quiz-card.js
@@ -4,11 +4,28 @@ console.clear();
 const bookmark = document.querySelector('[data-js="bookmark-button"]');
 const bookmarkContainer = document.querySelector('[data-js="bookmark-button"]');
 
-const showAnswerBtn = document.querySelectorAll('[data-js="answer-button"]');
+const showAnswerBtn = document.querySelector('[data-js="answer-button"]');
 const answer = document.querySelector('[data-js="answerField"]');
+
+bookmarkContainer.addEventListener("click", () => {
+  bookmark.classList.toggle("question-card--container__bookmark--clicked");
+});
 
 showAnswerBtn.addEventListener("click", (event) => {
   answer.classList.toggle("show-answer");
   showAnswerBtn.textContent =
     showAnswerBtn.textContent === "hide answer" ? "show answer" : "hide answer";
 });
+
+async function fetchCharacter() {
+  const response = await fetch("https://rickandmortyapi.com/api/character");
+  const data = await response.json();
+  const results = data.results;
+  const charactersNames = results.map((character) => {
+    const names = character.names;
+    return names;
+  });
+}
+fetchCharacter();
+
+console.log(fetchCharacter);
